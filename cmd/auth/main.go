1533be8054003936bddaace1bb881e4346e14557
Vladimir Mishakov <01.vladimir.mishakov@gmail.com>
2022-06-26 22:17:53 +0300

    Add structured logging.

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"gitlab.com/g6834/team41/auth/internal/app"
 	"gitlab.com/g6834/team41/auth/internal/env"
 )
 
 func main() {
+	env.E().L.SetFormatter(&logrus.JSONFormatter{})
+	env.E().L.SetReportCaller(true)
+
 	env.E().L.Info("starting service...")
-	env.E().M.StartServiceCounter.Inc()
 
 	defer env.OnStop()
 	//TODO gracefull shutdown app
